5052c0995c48770d38f156e7a4a798ab8022b489
soulbone <soulbone@users.noreply.github.com>
2015-11-28 04:51:00 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var uniqueRandomArr = require('unique-random-arr');
-var playlist = require('./playlist.plst');
+var uniqueRandomArr = require('unique-random-arr'); //random list 
+var playlist = require('./playlist.pl'); //playlist will be saved in a file called playlist.pl
 
 module.exports= {all:playlist,
 randon:uniqueRandomArr(playlist)};
